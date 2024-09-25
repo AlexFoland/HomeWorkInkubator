2c56fd4a0701fae16f3fddd47cd0c516905b54d0
alex folomeev <95583206+AlexFoland@users.noreply.github.com>
2024-09-25 23:38:46 +0600

    TaskTuesdayThree

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import s from './App.module.css'
-import HW1 from '../s2-homeworks/hw01/HW1'
+import HW3 from '../s2-homeworks/hw03/HW3'
 
 function App() {
     return (
         <div className={s.App}>
-            <HW1/>
+            <HW3/>
             {/*раскомментировать по ходу выполнения*/}
             {/*<HW2/>*/}
             {/*<HW3/>*/}
