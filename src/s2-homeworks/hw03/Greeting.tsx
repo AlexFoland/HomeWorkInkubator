09104f6a674a35e20d54e42130168d2cb09f033e
alex folomeev <95583206+AlexFoland@users.noreply.github.com>
2024-09-30 14:05:48 +0600

    TaskTuesdayThree Done

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -1,78 +1,76 @@
-import React, {ChangeEvent, KeyboardEvent} from 'react'
-import s from './Greeting.module.css'
+import React, { ChangeEvent, KeyboardEvent } from "react";
+import s from "./Greeting.module.css";
 
 type GreetingPropsType = {
-    name: string // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: (user:string)=>void // need to fix any
-    onBlur: any // need to fix any
-    onEnter: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
-    lastUserName?: any // need to fix any
-}
+  name: string; // need to fix any
+  setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void; // need to fix any
+  addUser: (user: string) => void; // need to fix any
+  onBlur: () => void; // need to fix any
+  onEnter: (e: KeyboardEvent<HTMLInputElement>) => void; // need to fix any
+  error: string; // need to fix any
+  totalUsers: number; // need to fix any
+  lastUserName?: string; // need to fix any
+};
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {
-        name,
-        setNameCallback,
-        addUser,
-        onEnter,
-        onBlur,
-        error,
-        totalUsers,
-        lastUserName,
-    } // деструктуризация пропсов
+  {
+    name,
+    setNameCallback,
+    addUser,
+    onEnter,
+    onBlur,
+    error,
+    totalUsers,
+    lastUserName,
+  } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (? s.errorInput : s.input)
+  const inputClass = s.errorInput ? s.errorInput : s.input;
 
-    return (
-        <div id={'hw3-form'} className={s.greetingForm}>
-            <div className={s.text}>
-                {'Людей добавили: '}
-                <span id={'hw3-users-total'}>
-                    {totalUsers}
-                </span>
-            </div>
+  return (
+    <div id={"hw3-form"} className={s.greetingForm}>
+      <div className={s.text}>
+        {"Людей добавили: "}
+        <span id={"hw3-users-total"}>{totalUsers}</span>
+      </div>
 
-            <div className={s.inputAndButtonContainer}>
-                <div>
-                    <input
-                        id={'hw3-input'}
-                        value={name}
-                        onChange={setNameCallback}
-                        className={inputClass}
-                        onKeyDown={onEnter}
-                        onBlur={onBlur}
-                    />
-                    <div id={'hw3-error'} className={s.error}>
-                        {error}
-                    </div>
-                </div>
-
-                <button
-                    id={'hw3-button'}
-                    onClick={()=>addUser(name)}
-                    className={s.button}
-                    disabled={!name.trim()}
-                    // ДАВАЙТЕ ПРОСЛЕДИМ БОЕВОЙ ПУТЬ addUser:
-                    // ОТСЮДА ОН ВСПЛЫВЕТ В КОМПОНЕНТЕ GreetingContainer И ВЫЗОВЕТ pureAddUser->
-                    // А В pureAddUser ЛИБО ВЫДАСТ ОШИБКУ (ЕСЛИ ПУСТОЙ name) ИЛИ ЗАПУТСИТ addUserCallback->
-                    // КОТОРЫЙ ВСПЛЫВЕТ В КОМПОНЕНТЕ <HW3/> И ВЫЗОВЕТ pureAddUserCallback->
-                    // КОТОРЫЙ СОЗДАСТ НОВЫЙ ОБЪЕКТ И ЗАСЕТАЕТ ЕГО В users НЕ ПОТЕРЯВ И СТАРЫХ ЮЗЕРОВ
-                >
-                    add
-                </button>
-            </div>
-
-            {lastUserName && (
-                <div className={s.greeting}>
-                    Привет <span id={'hw3-last-user'}>{lastUserName}</span>!
-                </div>
-            )}
+      <div className={s.inputAndButtonContainer}>
+        <div>
+          <input
+            id={"hw3-input"}
+            value={name}
+            onChange={setNameCallback}
+            className={inputClass}
+            onKeyDown={onEnter}
+            onBlur={onBlur}
+          />
+          <div id={"hw3-error"} className={s.error}>
+            {error}
+          </div>
         </div>
-    )
-}
 
-export default Greeting
+        <button
+          id={"hw3-button"}
+          onClick={() => addUser(name)}
+          className={s.button}
+          disabled={!name.trim()}
+          // ДАВАЙТЕ ПРОСЛЕДИМ БОЕВОЙ ПУТЬ addUser:
+          // ОТСЮДА ОН ВСПЛЫВЕТ В КОМПОНЕНТЕ GreetingContainer И ВЫЗОВЕТ pureAddUser->
+          // А В pureAddUser ЛИБО ВЫДАСТ ОШИБКУ (ЕСЛИ ПУСТОЙ name) ИЛИ ЗАПУТСИТ addUserCallback->
+          // КОТОРЫЙ ВСПЛЫВЕТ В КОМПОНЕНТЕ <HW3/> И ВЫЗОВЕТ pureAddUserCallback->
+          // КОТОРЫЙ СОЗДАСТ НОВЫЙ ОБЪЕКТ И ЗАСЕТАЕТ ЕГО В users НЕ ПОТЕРЯВ И СТАРЫХ ЮЗЕРОВ
+        >
+          add
+        </button>
+      </div>
+
+      {lastUserName && (
+        <div className={s.greeting}>
+          Привет <span id={"hw3-last-user"}>{lastUserName}</span>!
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Greeting;
